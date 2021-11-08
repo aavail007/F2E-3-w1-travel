c1eff265830e8720c99db561b6c3948dcf77cb24
limeng <limeng.chen@flowring.com>
2021-11-08 22:51:17 +0800

    feat: header

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <header class="w-full h-6 bg-white">
-123
+  <header class="w-full bg-white py-5 px-10 flex justify-between">
+    <img class="h-8" src="@/assets/img/logo.png" />
+    <div class="flex ">
+      <div class="mx-5">特色活動</div>
+      <div class="mx-5">美食饗宴</div>
+      <div class="mx-5">精選住宿</div>
+    </div>
+    <div>搜尋</div>
   </header>
 </template>
 
